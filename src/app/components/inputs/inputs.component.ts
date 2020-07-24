8b368ed2ff7e8cb2399d1664197996d84ef3636e
Wade Fleming <wadefleming2000@hotmail.com>
2020-07-24 23:04:34 +0100

    add empty inputs component

---
+++ src/app/components/inputs/inputs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inputs',
+  templateUrl: './inputs.component.html',
+  styleUrls: ['./inputs.component.less']
+})
+export class InputsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
