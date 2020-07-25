a52744b4e70806be413da48abecc69a8ecfeaf2e
Wade Fleming <wadefleming2000@hotmail.com>
2020-07-25 21:18:23 +0100

    added empty progress component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component'
 import { InputsComponent } from './components/inputs/inputs.component';
-import { LoadTesterComponent } from './components/load-tester/load-tester.component'
+import { LoadTesterComponent } from './components/load-tester/load-tester.component';
+import { ProgressComponent } from './components/progress/progress.component'
 
 @NgModule({
-    declarations: [AppComponent, InputsComponent, LoadTesterComponent],
+    declarations: [AppComponent, InputsComponent, LoadTesterComponent, ProgressComponent],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
