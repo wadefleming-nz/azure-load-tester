5e7cf22f3de657918db1f561936c1eef03a15641
Wade Fleming <wadefleming2000@hotmail.com>
2020-07-26 11:52:24 +0100

    add SolveStartResponse

---
+++ src/app/models/solve-start-response.model.ts
@@ -0,0 +1,7 @@
+export interface SolveStartResponse {
+    id: string;
+    statusQueryGetUri: string;
+    sendEventPostUri: string;
+    terminatePostUri: string;
+    purgeHistoryDeleteUri: string;
+}
