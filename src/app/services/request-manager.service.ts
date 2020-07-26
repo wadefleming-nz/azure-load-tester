48e67219e2efed92f649e41a8bb751860f4dc95f
Wade Fleming <wadefleming2000@hotmail.com>
2020-07-26 11:51:04 +0100

    use share() to prevent multiple requests

--- src/app/services/request-manager.service.ts
+++ src/app/services/request-manager.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, of } from 'rxjs';
-import { switchMap, catchError, filter, map } from 'rxjs/operators';
+import { switchMap, catchError, filter, map, share } from 'rxjs/operators';
 import { TestInstance } from '../models/test-instance.model';
 
 @Injectable({
@@ -17,7 +17,8 @@ export class RequestManagerService {
                 catchError((err) => of(`Error: ${JSON.stringify(err)}`)),
                 map((result) => JSON.stringify(result))
             )
-        ) // TODO use share
+        ),
+        share()
     );
 
     constructor(private http: HttpClient) {}
