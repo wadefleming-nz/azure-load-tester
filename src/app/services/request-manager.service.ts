a993a28851ffa05efe3e8d5748f16cc08e3a87ac
Wade Fleming <wadefleming2000@hotmail.com>
2020-07-26 12:17:23 +0100

    call status endpoint after start

--- src/app/services/request-manager.service.ts
+++ src/app/services/request-manager.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, of } from 'rxjs';
 import { switchMap, catchError, filter, map, share } from 'rxjs/operators';
 import { TestInstance } from '../models/test-instance.model';
 import { SolveStartResponse } from '../models/solve-start-response.model';
+import { SolveStatusResponse } from '../models/solve-status-response.model';
 
 @Injectable({
     providedIn: 'root',
@@ -14,9 +15,18 @@ export class RequestManagerService {
     testInstanceResult$ = this.testInstanceSubject.pipe(
         filter((ti) => !!ti),
         switchMap((ti) =>
-            this.http
-                .get<SolveStartResponse>(ti.endpointUrl)
-                .pipe(map((result) => JSON.stringify(result)))
+            this.http.get<SolveStartResponse>(ti.endpointUrl).pipe(
+                switchMap((startResponse) => {
+                    const statusUrl = startResponse.statusQueryGetUri;
+                    return this.http
+                        .get<SolveStatusResponse>(statusUrl)
+                        .pipe(
+                            map(
+                                (statusResponse) => statusResponse.runtimeStatus
+                            )
+                        );
+                })
+            )
         ),
         catchError((err) => of(`Error: ${JSON.stringify(err)}`)),
         share()
