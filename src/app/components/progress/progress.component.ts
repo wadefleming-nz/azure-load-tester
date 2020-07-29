a3a7a723c39af6e84f2913813ab98f5277c79e81
Wade Fleming <wadefleming2000@hotmail.com>
2020-07-29 22:49:39 +0100

    show results on graph

--- src/app/components/progress/progress.component.ts
+++ src/app/components/progress/progress.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { RequestManagerService } from 'src/app/services/request-manager.service';
-import { map, scan, filter } from 'rxjs/operators';
-import { RequestMetrics } from '../../models/request-metrics.model';
-import { ChartableRequestMetrics } from '../../models/chartable-request-metrics.model';
-import { of } from 'rxjs';
 
 @Component({
     selector: 'app-progress',
@@ -14,39 +10,7 @@ export class ProgressComponent {
     timeAxisMaxSeconds = 300;
     visibleRange = [0, this.timeAxisMaxSeconds];
 
-    data$ = of([]);
-    // this.requestManager.testInstanceResults$.pipe(
-    //     filter((result) => !!result),
-    //     scan(
-    //         (acc, value) => ({ ...acc, currTime: new Date(), status: value }),
-    //         {
-    //             id: 1,
-    //             startTime: new Date(),
-    //             currTime: new Date(),
-    //             status: 'None',
-    //         }
-    //     ),
-    //     map((status) => [this.createChartableRequestMetrics(status)])
-    // );
+    data$ = this.requestManager.testInstanceResults$;
 
-    constructor(public requestManager: RequestManagerService) {
-        this.data$.subscribe(console.log);
-    }
-
-    private createChartableRequestMetrics(
-        requestMetrics: RequestMetrics
-    ): ChartableRequestMetrics {
-        return {
-            id: requestMetrics.id,
-            elapsedSeconds: this.getElapsedSecondsBetween(
-                requestMetrics.startTime,
-                requestMetrics.currTime
-            ),
-        };
-    }
-
-    private getElapsedSecondsBetween(date1: Date, date2: Date) {
-        const milliSecondsDiff = date2.getTime() - date1.getTime();
-        return milliSecondsDiff / 1000;
-    }
+    constructor(public requestManager: RequestManagerService) {}
 }
