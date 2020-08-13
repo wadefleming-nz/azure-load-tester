ca437503bf638ec1e60032479e3f0e2f0d4ea63d
Kavitha Desu <kdesu@caci.co.uk>
2020-08-13 14:59:34 +0100

    changes to service to pass payload.

--- src/app/components/progress/progress.component.ts
+++ src/app/components/progress/progress.component.ts
@@ -9,7 +9,7 @@ import { FunctionStatus } from '../../enums/function-status.enum';
     styleUrls: ['./progress.component.less'],
 })
 export class ProgressComponent {
-    timeAxisMaxSeconds = 100;
+    timeAxisMaxSeconds = 1000;
     visibleRange = [0, this.timeAxisMaxSeconds];
 
     data$ = this.requestManager.testInstanceResults$;
