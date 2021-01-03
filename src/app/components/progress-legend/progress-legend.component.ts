c0e13be23efbd89f8c97824ee2508c8b8fbb1891
Wade Fleming <wadefleming2000@hotmail.com>
2021-01-03 18:29:41 +0200

    use actual colors instead of labels

--- src/app/components/progress-legend/progress-legend.component.ts
+++ src/app/components/progress-legend/progress-legend.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { statusColors } from 'src/app/constants/status-colors.constant';
 
 @Component({
     selector: 'app-progress-legend',
     templateUrl: './progress-legend.component.html',
     styleUrls: ['./progress-legend.component.less'],
 })
-export class ProgressLegendComponent {}
+export class ProgressLegendComponent {
+    statusColors = statusColors;
+}
